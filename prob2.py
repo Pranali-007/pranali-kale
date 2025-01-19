6711247b9dfe49a40cd4cb1f362ea47f5117e649
Pranali-007 <kalepranali74@gmail.com>
2025-01-19 15:42:29 +0530

    python

---
+++ prob2.py
@@ -0,0 +1,6 @@
+# install external module and use it to perform operation
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("my name is pranali kale ,i am from pune and studing in engineering college")
+engine.runAndWait()
+# by using pip install pyttsx3
