a094de17738c5c99d289ad77a201797348bdfc03
pranali kale <kalepranali74@gmail.com>
2025-01-21 18:54:43 +0530

    Add files via upload
    string slicing in python

---
+++ pb1.py
@@ -0,0 +1,15 @@
+#user name followed by good afternoon using input()function
+
+a = input("enter the user name: ")
+print("good afternoon",a)
+#print (f"good afternoon {a})
+
+#fill date and name in given syntax
+
+letter =''' Dear <|Name|>,
+you are selected in good position with 9LPA on|
+<|Date|>'''
+
+print(letter.replace("<|Name|>","pranali").replace("<|Date|","7 aug 2025"))
+
+      
---
+++ pb2.py
@@ -0,0 +1,12 @@
+#to detect double space
+name = "pranali kale name hai mera  "  # if o/p=-1.....no double space and if o/p= any number......double space
+print(name.find("  "))  
+
+# to replace value
+print(name.replace(" ","  ")) # double place get replace with single place
+
+# strings are immutable means we can naot change while executing and running any function on it.  
+
+letter = "hello pranali, \n\t you are such a good person\n thanks!"  #\t tap for space
+print(letter)
+
